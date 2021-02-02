7c1134ffd482b455769b5687a9154ea87267c4b8
Fifth Patient <1923407743@qq.com>
2021-02-02 21:47:05 +0800

    feat 001-file express /api/file

--- 001-file/src/app.js
+++ 001-file/src/app.js
@@ -5,4 +5,37 @@ const path = require('path')
 
 app.use('/', express.static(path.join(__dirname, './static')))
 
+app.get('/api/file', (req, res) => {
+  const { type } = req.query
+  let imgURL = path.join(__dirname, './file/logo.png')
+
+  if (type && type !== '') {
+    const fs = require("fs");
+    const mineType = require('mime-types')
+    try {
+      let imageData = fs.readFileSync(imgURL)
+      let bufferData = Buffer.from(imageData).toString('base64')
+      let base64 = "data:" + mineType.lookup(imgURL) + ';base64,' + bufferData
+      if (type === 'content') {
+        // 响应文件内容
+        res.send(bufferData)
+      } else {
+        // 响应data url base64
+        res.send(base64)
+      }
+    } catch (error) {
+      console.error(error)
+      res.status(502).send(error)
+    }
+  } else {
+    // 直接响应文件
+    res.download(imgURL, 'logo.png')
+  }
+})
+
+app.get('/api/file-download', (req, res) => {
+  res.send('/api/file-download')
+})
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
