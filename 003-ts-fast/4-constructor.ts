852570d21ce489b8c40fc4869ceeaffeda0c2e7a
Fifth Patient <1923407743@qq.com>
2021-12-24 12:05:23 +0800

    Update 4-constructor.ts

--- 003-ts-fast/4-constructor.ts
+++ 003-ts-fast/4-constructor.ts
@@ -1,7 +1,20 @@
-let Bool: Boolean = new Boolean()
-let bool: boolean = true
+let newBool: new String = new String()
+let StrFn: Function = String
+let Str: String = String()
+let str: string = ''
 
-Bool = bool
-bool = Boolean
+Str = StrFn
+StrFn = Str
+
+Str = new String()
+Str = str
+
+str = Str
+str = new String()
+str = true
+
+str = new String()
+str = new String('').toString()
+str = `${new String('')}`
 
 export {}
