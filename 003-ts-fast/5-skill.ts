5c30f0c54435f354e4422f108014b632b2116e7d
Fifth Patient <1923407743@qq.com>
2021-12-24 12:44:07 +0800

    update 5-skill

---
+++ 003-ts-fast/5-skill.ts
@@ -0,0 +1,19 @@
+/* 5-skill.ts */
+
+type myStr = string
+
+type strOrNum = string | number
+
+type oneToThre = '' | 1 | 2 | 3 | strOrNum
+
+let myStr: myStr = ''
+myStr = 'myStr'
+myStr = 1
+
+let strOrNum: strOrNum = ''
+strOrNum = 1
+
+let oneToThre: oneToThre = ''
+oneToThre = 1
+oneToThre = 2
+oneToThre = 3
