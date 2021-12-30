a6599e6f309df762f8a6ed63d677bc97d444fb79
screw-hand <screwhand0@gmail.com>
2021-12-31 00:26:33 +0800

    005-webpack-init plugin

--- 005-webpack-init/webpack.config.js
+++ 005-webpack-init/webpack.config.js
@@ -1,3 +1,4 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const path = require('path')
 
 module.exports = {
@@ -10,17 +11,28 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          // 'css-loader?minimize'
-          {
-            loader: 'css-loader',
-            options: {
-              minimize: true
-            }
-          }
-        ]
+        // use: [
+        //   // 'style-loader',
+        //   // // 'css-loader?minimize'
+        //   // {
+        //   //   loader: 'css-loader',
+        //   //   options: {
+        //   //     minimize: true
+        //   //   }
+        //   // }
+        //   ExtractTextPlugin.extract({
+        //     use: ['css-loader']
+        //   })
+        // ]
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader']
+        })
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name]_[contenthash:8].css'
+    })
+  ]
 }
