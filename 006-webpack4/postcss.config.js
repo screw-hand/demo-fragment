ed32f2f631a47f55750afafe496091fdfdea3a6f
Fifth Patient <1923407743@qq.com>
2022-01-06 14:31:48 +0800

    006-webpack4 duplicate plugin autoprefixer

--- 006-webpack4/postcss.config.js
+++ 006-webpack4/postcss.config.js
@@ -1,13 +1,14 @@
-const autoprefixer = require('autoprefixer');
+// const autoprefixer = require('autoprefixer');
 const stylelint = require('stylelint');
 const postcssCssnext = require('postcss-cssnext');
 
 module.exports = {
   plugins: [
-    autoprefixer({
+    /* Warning: postcss-cssnext found a duplicate plugin ('autoprefixer') in your postcss plugins. This might be inefficient. You should remove 'autoprefixer' from your postcss plugin list since it's already included by postcss-cssnext. */
+    /* autoprefixer({
       grid: true,
       browsers: ['> 1%', 'last 3 versions', 'android 4.2', 'ie 8'],
-    }),
+    }), */
     stylelint({
       config: {
         rules: {
