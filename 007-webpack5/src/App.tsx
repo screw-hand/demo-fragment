6cf4f8d87a98315e93c094134afcb10baff304b9
screw-hand <screwhand0@gmail.com>
2022-01-27 15:45:43 +0800

    007-webpack5 update App.tsx

--- 007-webpack5/src/App.tsx
+++ 007-webpack5/src/App.tsx
@@ -1,10 +1,16 @@
 import Style from './App.styl'
+import HelloMessage from './multi-page/HelloMessage'
 import './script'
 
 const cname = Style.root
 
 const App = () => {
-  return <h1 className={cname}>Hello,Webpack</h1>
+  return (
+    <div>
+      <HelloMessage name='1' />
+      <h1 className={cname}>Hello,Webpack</h1>
+    </div>
+  )
 }
 
 export default App
