e10882f6eb95964a6154c32666dab298f0f61ded
screw-hand <screwhand0@gmail.com>
2022-03-10 18:52:27 +0800

    Update script.js

--- 000-temp/learn-javascript/script.js
+++ 000-temp/learn-javascript/script.js
@@ -2,13 +2,31 @@ function main() {
   let result = {};
 
   // code start
+  var Foo = function () {
+    this.bar = "bar";
+  };
+  var foo = new Foo();
+
+  result.typeofFoo = typeof Foo;
+  result.typeof_foo = typeof foo;
   // code end
 
   return result;
 }
 
 const inputCode = main.toString();
-const outputCode = (main()).valueOf();
+const outputObj = main().valueOf();
+const outputCode =
+  "{" +
+  "\n" +
+  Object.keys(outputObj)
+    .map((k) => {
+      const v = outputObj[k];
+      return `  ${k}: ${v}`;
+    })
+    .join("\n") +
+  "\n" +
+  "}";
 const inputCodeDom = document.getElementById("input");
 const outputCodeDom = document.getElementById("output");
 inputCodeDom.innerHTML = inputCode;
