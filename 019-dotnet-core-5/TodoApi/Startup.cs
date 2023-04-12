ec0e098bde0728a961951cac1dbd95b7dadcef65
screw-hand <screwhand0@gmail.com>
2023-04-12 11:45:10 +0800

    019-dotnet-core-5 TodoAPI

--- 019-dotnet-core-5/TodoApi/Startup.cs
+++ 019-dotnet-core-5/TodoApi/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 
 namespace TodoApi
 {
@@ -26,6 +28,9 @@ namespace TodoApi
         {
 
             services.AddControllers();
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList")
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
