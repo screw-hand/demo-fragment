c828172e6d7ef0a730d9d435c6880fc0fcaa1e20
screw-hand <screwhand0@gmail.com>
2023-04-30 22:14:52 +0800

    020-java-design-pattern facory

--- 020-java-design-pattern/_01Factory/_01Factory.java
+++ 020-java-design-pattern/_01Factory/_01Factory.java
@@ -2,11 +2,9 @@ package _01Factory;
 
 /**
  * 工厂方法
- * 
- * @deprecated
- *             定义一个用于创建对象的接口
- *             让子类去决定实例化哪一个类
- *             Factory Method使一个类的实例化延迟到子类
+ * 定义一个用于创建对象的接口
+ * 让子类去决定实例化哪一个类
+ * Factory Method使一个类的实例化延迟到子类
  */
 
 interface Product {
