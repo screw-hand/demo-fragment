c1ce2c8d147fb968eab3d2066b9cfff0bc005a0c
screw-hand <screwhand0@gmail.com>
2023-06-06 10:38:00 +0800

    022-vitesse-app clear

--- 022-vitesse-app/src/layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <view class="px-10 py-20 text-center">
-    <slot />
-    <Footer />
-    <view class="mx-auto mt-5 text-center text-sm opacity-25">
-      [Default Layout]
-    </view>
-  </view>
-</template>
